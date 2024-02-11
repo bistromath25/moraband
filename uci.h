e87bdd309197744bbb57225bc9dd23269ff8e983
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2024-02-11 13:23:54 -0500

    Test utils, probcut, razoring (#13)
    * Testing utilities, tune eval
    
    * razor, probcut, tune eval
    
    * tt
    
    * Update CMakeLists.txt
    
    * tt.cpp
    
    * update tt.cpp
    
    * std int types in defs
    
    * more test positions
    
    * research on pv size 0
    
    * threads option in testing utils
    
    * fix illegal move gen bug, check pseudo legal king move
    
    * cleanup
    
    * update test positions
    
    * Prune based on eval history (#17)
    
    * prune based on past eval history
    
    * trigger gha
    
    * fix
    
    * Code formatting (#18)
    
    * clang-format
    
    * end newline
    
    * return pseudo checkmate in qsearch if all moves are bad

--- uci.h
+++ uci.h
@@ -6,19 +6,19 @@
 #ifndef UCI_H
 #define UCI_H
 
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <utility>
-#include "position.h"
-#include "tt.h"
 #include "defs.h"
 #include "movegen.h"
+#include "position.h"
 #include "search.h"
 #include "timeman.h"
+#include "tt.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
 
 #define ENGINE_NAME "Moraband"
-#define ENGINE_VERSION "1.1.2"
+#define ENGINE_VERSION "1.2"
 #define ENGINE_AUTHOR "Brighten Zhang"
 
 extern int HASH_SIZE;
@@ -26,7 +26,7 @@ extern int NUM_THREADS;
 extern int MOVE_OVERHEAD;
 extern int CONTEMPT;
 
-void bench(int depth=16);
+void bench(int depth = 16);
 void uci();
 
 #endif
--- zobrist.cpp
+++ zobrist.cpp
@@ -6,29 +6,29 @@
 #include "zobrist.h"
 
 namespace Zobrist {
-	U64 piece_rand[PLAYER_SIZE][PIECE_TYPES_SIZE][BOARD_SIZE];
-	U64 ep_file_rand[8];
-	U64 castle_rand[16];
-	U64 side_to_move_rand;
+    U64 piece_rand[PLAYER_SIZE][PIECE_TYPES_SIZE][BOARD_SIZE];
+    U64 ep_file_rand[8];
+    U64 castle_rand[16];
+    U64 side_to_move_rand;
 
-	U64 rand_64() {
-		return U64(rand()) << 32 | U64(rand());
-	}
+    U64 rand_64() {
+        return U64(rand()) << 32 | U64(rand());
+    }
 
-	void init() {
-		srand(6736199);
-		for (PieceType p = PIECETYPE_PAWN; p < PIECETYPE_NONE; ++p) {
-			for (Square s = first_sq; s <= last_sq; ++s) {
-				piece_rand[WHITE][p][s] = rand_64();
-				piece_rand[BLACK][p][s] = rand_64();
-			}
-		}
-		for (File f = file_a; f <= file_f; ++f) {
-			ep_file_rand[f] = rand_64();
-		}
-		for (int i = 0; i < 16; ++i) {
-			castle_rand[i] = rand_64();	
-		}
-		side_to_move_rand = rand_64();
-	}
-};
+    void init() {
+        srand(6736199);
+        for (PieceType p = PIECETYPE_PAWN; p < PIECETYPE_NONE; ++p) {
+            for (Square s = first_sq; s <= last_sq; ++s) {
+                piece_rand[WHITE][p][s] = rand_64();
+                piece_rand[BLACK][p][s] = rand_64();
+            }
+        }
+        for (File f = file_a; f <= file_f; ++f) {
+            ep_file_rand[f] = rand_64();
+        }
+        for (int i = 0; i < 16; ++i) {
+            castle_rand[i] = rand_64();
+        }
+        side_to_move_rand = rand_64();
+    }
+}; // namespace Zobrist
